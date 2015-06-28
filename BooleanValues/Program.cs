2c0d57b41f5b1f2f20ec4fbf0ab11c6d210ad66c
David Lowery <dalowery@dalsystems.com>
2015-06-27 22:22:51 -0500

    BooleanValues

---
+++ BooleanValues/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool myFirstBoolean = 3 + 2 == 5;
+            Console.WriteLine(myFirstBoolean);
+
+            bool mySecondBoolean = 3 + 2 > 5;
+            Console.WriteLine(mySecondBoolean);
+
+            bool bothAreTrue = myFirstBoolean && mySecondBoolean;
+            Console.WriteLine("bothAreTrue evaluates " + bothAreTrue);
+
+            bool oneIsTrue = myFirstBoolean || mySecondBoolean;
+            Console.WriteLine("oneIsTrue evaluates {0} because one expression is true", oneIsTrue);
+
+            bool reverseMyFirstBoolean = !myFirstBoolean;
+            Console.WriteLine("myFirstBoolean is {0} but reverseMyFirstBoolean is {1}", myFirstBoolean, reverseMyFirstBoolean);
+        }
+    }
+}
