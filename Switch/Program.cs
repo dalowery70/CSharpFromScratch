6e43d7cce4fd11c48fae83f1b24d07e3f1c87953
David Lowery <dalowery@dalsystems.com>
2015-06-27 23:15:20 -0500

    Switch

---
+++ Switch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Pick a number between 1 and 5: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 5:
+                    Console.WriteLine("Correct");
+                    break;
+                case 4:
+                case 3:
+                case 2:
+                case 1:
+                    Console.WriteLine("Nope!");
+                    break;
+                default:
+                    Console.WriteLine("Not a legal entry. Please try again.");
+                    break;
+            }
+        }
+    }
+}
