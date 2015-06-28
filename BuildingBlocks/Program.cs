17823eb86fda2333f517a8e87d028843fd57ee89
David Lowery <dalowery@dalsystems.com>
2015-06-27 19:45:59 -0500

    Create

---
+++ BuildingBlocks/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myVariable = "Variable";
+            string myString = "String";
+            Console.WriteLine(myVariable + " " + myString);
+
+            int myFirstValue = 23;
+            double mySecondValue = 45.79;
+            double sum = myFirstValue + mySecondValue;
+            Console.WriteLine(sum.ToString());
+        }
+    }
+}
