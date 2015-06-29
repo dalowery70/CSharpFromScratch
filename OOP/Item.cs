fc55f79525ce39e02ac92d73e1ebddbc9cb6b4ed
David Lowery <dalowery@dalsystems.com>
2015-06-28 20:02:01 -0500

    OOP with Inheritance

--- OOP/Item.cs
+++ OOP/Item.cs
@@ -1,8 +1,48 @@
-﻿namespace OOP
+﻿using System;
+
+namespace OOP
 {
-    public class Item
+    class Item
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public virtual void Purchase()
+        {
+            Console.WriteLine("Purchasing {0}", Name);
+        }
+    }
+
+    // Normally this would be one class per file - shown this way for simplicity
+
+    class Software : Item // Inherits from Item (base-class means more general form). This means software already has an ID and Name
+    {
+        public string ISBN { get; set; }
+        public override void Purchase()
+        {
+            base.Purchase();
+            Console.WriteLine("My ID is {0} and my ISBN is {1}", ID, ISBN);
+        }
+
+    }
+
+    class Hardware : Item
+    {
+        public string SerialNumber { get; set; }
+    }
+
+    class Computer : Hardware
+    {
+        public string CPUType { get; set; }
+        public string Disks { get; set; }
+        public override void Purchase()
+        {
+            base.Purchase();
+            Console.WriteLine("myCPUType is {0}", CPUType);
+        }
+    }
+
+    class Peripheral : Hardware
+    {
+        public string Description { get; set; }
     }
 }
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -13,6 +13,23 @@ namespace OOP
             var newWarehouse = new Warehouse("Warehouse1", 101);
             var myItem = newWarehouse.FindAndReturnItem(101);
             Console.WriteLine("My new item is {0}", myItem.Name);
+
+            var myComputer = new Computer();
+            myComputer.ID = 102;
+
+            Item mySecondComputer = new Computer(); // only get properties from Item
+            mySecondComputer.ID = 103;
+            Computer mySecondComputer2 = mySecondComputer as Computer; // treat AS Computer, get all Computer properties
+            mySecondComputer2.CPUType = "i7";
+            string myCPUype = mySecondComputer2.CPUType;
+            mySecondComputer2.Name = "Jupiter";
+            mySecondComputer2.Purchase();
+
+            Software mySoftware = new Software();
+            mySoftware.Name = "Foo Software";
+            mySoftware.ID = 101;
+            mySoftware.ISBN = "1234";
+            mySoftware.Purchase();
         }
     }
 }
