e15df318a45eb22f70cd7659ec07c5dbd6cac097
David Lowery <dalowery@dalsystems.com>
2015-06-29 13:36:04 -0500

    Interfaces

---
+++ Interfaces/DBEntry.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public class DBEntry : Storable
+    {
+        private ReturnStatus status;
+        public ReturnStatus Status
+        {
+            get
+            {
+                return status;
+            }
+
+            set
+            {
+                status = value;
+            }
+        }
+
+        public string Read()
+        {
+            return "Reading entry from database";
+        }
+
+        public void Write(object obj)
+        {
+            Console.WriteLine("Writing obj to database...");
+        }
+    }
+}
---
+++ Interfaces/Note.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public class Note : Storable
+    {
+        private ReturnStatus status;
+        public ReturnStatus Status
+        {
+            get
+            {
+                return status;
+            }
+
+            set
+            {
+                status = value;
+            }
+        }
+
+        public string Read()
+        {
+            return "Reading note from disk";
+        }
+
+        public void Write(object obj)
+        {
+            Console.WriteLine("Writing obj to disk...");
+        }
+    }
+}
