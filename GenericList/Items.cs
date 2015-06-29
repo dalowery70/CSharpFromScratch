e35123300affa418a9b7a657010252da7e69c4cc
David Lowery <dalowery@dalsystems.com>
2015-06-28 23:37:42 -0500

    Lists

---
+++ GenericList/Items.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    class Item
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public virtual void Purchase()
+        {
+            Console.WriteLine("Purchasing {0}", Name);
+        }
+
+        public static List<Item> GetItems(int numToGet)
+        {
+            var random = new Random();
+            var newList = new List<Item>();
+            Item newItem;
+            for (int i = 0; i < numToGet; i++)
+            {
+                newItem = new Item() { ID = random.Next(), Name = "myItem" + i.ToString() };
+                newList.Add(newItem);
+            }
+            return newList;
+        }
+    }
+}
---
+++ GenericList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>();
+            var r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                myList.Add(r.Next());
+            }
+
+            myList.Sort();
+
+            foreach (int i in myList)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            List<Item> myItems = Item.GetItems(20);
+            foreach (Item item in myItems)
+            {
+                Console.WriteLine("New item ID = {0}, name = {1}", item.ID, item.Name);
+            }
+        }
+    }
+}
