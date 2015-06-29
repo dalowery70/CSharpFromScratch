15d2f9c098b3f984fc0718c440827b7cc8d48528
David Lowery <dalowery@dalsystems.com>
2015-06-29 14:00:35 -0500

    Delegates

---
+++ Delegates/MediaTester.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class MediaTester
+    {
+        public delegate int TestMedia();
+
+        public void RunTest(TestMedia testDelegate)
+        {
+            int result = testDelegate();
+            if (result == 0)
+                Console.WriteLine("Works!");
+            else
+                Console.WriteLine("Failed.");
+        }
+    }
+}
